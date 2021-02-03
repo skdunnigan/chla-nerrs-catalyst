e4bfe31371e8a0f8ef50aca377df4a929373e62a
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-02-03 10:32:15 -0500

    update load data

--- R/02_load_data.R
+++ R/02_load_data.R
@@ -1,20 +1,34 @@
+# ---- Read in ISCO data from files
 library(readxl)
+library(purrr)
+library(dplyr)
+library(janitor)
 
+# read in all data files as a list
 
-dat.files  <- list.files(path=here::here('data'),
-                         recursive=T,
-                         pattern="chla_template*"
-                         ,full.names=T)
+dat.files  <- list.files(path = here::here('data'),
+                         recursive = TRUE,
+                         pattern = "chla_template*",
+                         full.names = TRUE)
 
-readDatFile <- function(f) {
-  dat.fl <- readxl::read_xlsx(f, sheet = "isco") 
+readisco <- function(f) {
+  dat.is <- readxl::read_xlsx(f, sheet = "isco") 
 }
 
-data.files <- sapply(dat.files, readDatFile, simplify = FALSE) 
+readtank <- function(f) {
+  dat.ta <- readxl::read_xlsx(f, sheet = "tank") 
+}
 
-dat <- data.files[[2]]
+isco.data.files <- sapply(dat.files, readisco, simplify = FALSE)
+# isco.data.files2 <- purrr::map(dat.files, readisco)
 
+tank.data.files <- sapply(dat.files, readtank, simplify = FALSE)
 
-%>%
+# this should work when there are actually data populating the template
+isco.dat <- isco.data.files %>% 
+  dplyr::bind_rows(.id = "id") %>%
+  janitor::clean_names()
+
+tank.dat <- tank.data.files %>% 
   dplyr::bind_rows(.id = "id") %>%
   janitor::clean_names()
