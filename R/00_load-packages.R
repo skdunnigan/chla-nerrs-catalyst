fdd71f6d09d1a7396526a3c08bf4d69d4011ed44
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-06-04 16:56:29 -0400

    created load packages script

---
+++ R/00_load-packages.R
@@ -0,0 +1,11 @@
+# load packages -----------------------------------------------------------
+
+library(readxl)
+library(dplyr)
+library(janitor)
+library(tidyverse)
+library(ggpubr)
+library(readxl)
+library(here)
+library(cowplot)
+library(plotly)
