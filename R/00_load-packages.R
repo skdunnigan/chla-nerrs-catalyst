fcd5d77f717aa754e9be5295ecb767bb1fd8d451
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-08-05 14:35:21 -0400

    updated packages

--- R/00_load-packages.R
+++ R/00_load-packages.R
@@ -7,8 +7,11 @@ library(tidyverse)
 library(ggpubr)
 library(readxl)
 library(here)
-library(cowplot)
 library(plotly)
 library(scales)
 library(knitr)
 library(kableExtra) # https://cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_html.html 
+library(patchwork)
+library(broom)
+
+
