3b1fadb029906aec8bcf0e4aafe173575ae1970e
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-08-09 16:16:33 -0400

    updated tables and added new summary table

--- R/06_tables.R
+++ R/06_tables.R
@@ -1,57 +1,6 @@
-# create tables
 
-# params <- c('chla_ugl', 'chla_rfu', 'fdom_qsu', 'do_mgl', 'sal', 'turb', 'temp', 'p_h', 'chlorophyll_rfu')
-# 
-# for (i in 1:length(params)){
-#   
-#   min_max <- all %>% 
-#     group_by(reserve_code) %>% 
-#     select(reserve_code, chla_ugl, chla_rfu, fdom_qsu, do_mgl, sal, turb, temp, p_h, chlorophyll_rfu) %>% 
-#     summarise(across(everything(), list(min = min, max = max)))
-#   
-#   mean <- all %>% 
-#     group_by(reserve_code) %>% 
-#     select(reserve_code, chla_ugl, chla_rfu, fdom_qsu, do_mgl, sal, turb, temp, p_h, chlorophyll_rfu) %>% 
-#     summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)))
-#   
-#   sd <- all %>% 
-#     group_by(reserve_code) %>% 
-#     select(reserve_code, chla_ugl, chla_rfu, fdom_qsu, do_mgl, sal, turb, temp, p_h, chlorophyll_rfu) %>% 
-#     summarise(across(where(is.numeric), ~ sd(.x, na.rm = TRUE)))
-#   
-#   combo <- left_join(min_max, mean, by = "reserve_code") %>% left_join(sd, by = "reserve_code")
-#   
-#   tempdf <- combo %>% 
-#     select(reserve_code, contains(params[i])) %>% 
-#     dplyr::mutate(range = paste0(min))
-#   
-#   name <- params[i] # pull out the name you want of the file
-#   
-#   assign(paste0(name, "_", 'sum'), tempdf) 
-#   
-#   rm(tempdf)
-# }
-# 
-# a <- all %>% 
-#       group_by(reserve_code) %>% 
-#       summarise_all()
+# summary table for individual reserve and all runs -----------------------
 
-# reserve <- unique(all$reserve_code)
-# 
-# for (i in 1:length(reserve)){
-# 
-# tempdf <- all %>%
-#                       dplyr::filter(reserve_code == reserve[i]) %>% 
-#                       select(chla_ugl, chla_rfu, fdom_qsu, do_mgl, sal, turb, temp, p_h, chlorophyll_rfu) %>%
-#                       psych::describe(quant=c(.25,.75)) %>%
-#                       as_tibble(rownames="rowname")  %>%
-#                       print()
-#   name <- reserve[i] # pull out the name you want of the file
-#   
-#   assign(paste0(name, "_", 'sum'), tempdf) 
-#   
-#   rm(tempdf)
-# }
 
 reserve_sum_table_all <- function(site) {
   all %>%
@@ -64,6 +13,10 @@ reserve_sum_table_all <- function(site) {
 }
 
 # a <- reserve_sum_table('SAP')
+
+
+# summary table for individual reserve and only ISCO ----------------------
+
 
 # just isco data
 reserve_sum_table_isco <- function(site) {
@@ -78,10 +31,13 @@ reserve_sum_table_isco <- function(site) {
 
 # reserve_sum_table_isco("GTM")
 
+
+# summary table for individual reserve and only tank runs -----------------
+
 # just tank data
 reserve_sum_table_tank <- function(site) {
   tank %>%
-    dplyr::filter(reserve_code == site) %>% 
+    dplyr::filter(reserve_code == site & rep == 1) %>% 
     select(chla_ugl, chla_rfu, fdom_qsu, do_mgl, sal, turb, temp, p_h, chlorophyll_rfu) %>%
     psych::describe() %>%
     as_tibble(rownames="parameter")  %>%
@@ -90,3 +46,60 @@ reserve_sum_table_tank <- function(site) {
 }
 
 # reserve_sum_table_tank("GTM")
+
+
+# make summary table of all reserves --------------------------------------
+
+
+reserves <- unique(all$reserve_code)
+
+sum_tables <- list()
+
+for (i in 1:length(reserves)){
+
+# reserve_sum_table_all <- function(site) {
+  
+ tempdf <- all %>%
+             dplyr::filter(reserve_code == reserves[i]) %>% 
+             select(chla_ugl, chla_rfu, fdom_qsu, do_mgl, sal, turb, temp, p_h, chlorophyll_rfu) %>%
+             psych::describe() %>%
+             as_tibble(rownames="parameter")  %>%
+             select(-vars) 
+  
+  sum_tables[[i]] <- tempdf # add file to opened list()
+  
+  name <- reserves[i] # pull out the name you want of the file
+  
+  # names[[i]] <- paste(name)
+  
+  rm(tempdf)
+}
+
+names(sum_tables) <- paste0(c(reserves))
+
+sum_table_alpha <- purrr::map_df(sum_tables, ~as.data.frame(.x), .id="id")
+
+sum_table_beta <- sum_table_alpha %>% 
+                  dplyr::mutate(item = paste0(round(min, digits = 2), "-", round(max, digits = 2), " (", round(mean, digits = 2), "\U00B1", round(sd, digits = 2), ")"))
+
+sum_table_final <- sum_table_beta %>% 
+                    dplyr::select(id, parameter, item) %>% 
+                    dplyr::filter(item != 'Inf--Inf (NaN±NA)') %>% 
+                    tidyr::pivot_wider(id_cols = c('id'),
+                                       names_from = 'parameter',
+                                       values_from = 'item') %>% 
+                    dplyr::rename(Reserve = id,
+                                  `Extracted CHLA (ugL)` = chla_ugl,
+                                  `Extracted CHLA (RFU)` = chla_rfu,
+                                  `fDOM (QSU)` = fdom_qsu,
+                                  `Dissolved Oxygen (mg/L)` = do_mgl,
+                                  `Salinity (psu)` = sal,
+                                  `Turbidity (FNU)` = turb,
+                                  `Temperature (C)` = temp,
+                                  `pH` = p_h,
+                                  `CHLA (RFU) EXO` = chlorophyll_rfu)
+
+rm(sum_tables, 
+   sum_table_alpha, 
+   sum_table_beta,
+   reserves)
