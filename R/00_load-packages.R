2a844b788531d073510ede99d8722924e886d370
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-11-30 11:30:20 -0500

    tidy packages

--- R/00_load-packages.R
+++ R/00_load-packages.R
@@ -1,34 +1,34 @@
 # load packages -----------------------------------------------------------
 
 # be sure to check for packages conflicts!
-# import
+# import ----
 library(readxl) # read excel files
 library(janitor) # simple tools to clean dirty data
 library(here) # a simpler way to find your files
-library(SWMPr) # working with SWMP data from the NERRS
-library(SWMPrExtension) # expanded functions for SWMPr package
+# library(SWMPr) # working with SWMP data from the NERRS
+# library(SWMPrExtension) # expanded functions for SWMPr package
 
-# tidy and wrangle
+# tidy and wrangle ----
 library(tidyverse) # because...tidyverse (ggplot2, tidyr, dplyr)
 library(lubridate) # dates and times
 
-# stats
+# stats ----
 library(broom) # convert statistical analysis objects into tidy tibbles
 library(psych) # has expanded statistical functions
 
-# markdown
+# markdown ----
 library(knitr)
 library(rmarkdown)
 library(kableExtra)
 
-# expanded data visualization
+# expanded data visualization ----
 library(cowplot) # additional themes for ggplot2
-library(gridExtra) # grid graphics
+# library(gridExtra) # grid graphics
 library(patchwork) # grid graphics (better)
 library(scales) # scale functions for visualization
-library(gganimate) # make animated plots
+# library(gganimate) # make animated plots
 library(plotly) # create interactive web graphics - use for html output files
-library(ggcorrplot) # visualization of correlation matrix using ggplot2
+# library(ggcorrplot) # visualization of correlation matrix using ggplot2
 # or install the latest ggcorrplot from GitHub
 # Install
 # if(!require(devtools)) install.packages("devtools")
