96a9e0e0d0b78a388d0cb64d7e2faec85de0c8e7
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2022-04-05 14:04:20 -0400

    updated fdom and added bw version

--- R/interference/03_fdom-load-wrangle-run.R
+++ R/interference/03_fdom-load-wrangle-run.R
@@ -152,6 +152,7 @@ stds_gtm <- dplyr::bind_rows(
 
 # 02 plotting ----  
 
+# color
 a <- DI_fdom %>% 
       ggplot(aes(x = avg_fdom_qsu, y = corrected_avg_chl_rfu,
                  color = standard,
@@ -209,4 +210,89 @@ fdom_multiplot <- ((a +
      labs(title = "C")) +
   plot_annotation(caption = "Note different scales for y axis.")
   
+# b&w
 
+a_bw <- DI_fdom %>% 
+        ggplot(aes(x = avg_fdom_qsu, y = corrected_avg_chl_rfu,
+                   linetype = standard,
+                   shape = standard,
+                   group = standard_run)) +
+        geom_point() +
+  # xlim(0,300) +
+        stat_smooth(method = "lm",
+                    se = FALSE,
+                    fullrange = F,
+                    color = "black",
+                    size = 0.5) +
+        scale_shape_manual(name = "Standard", values = standard_shapes) +
+        scale_linetype_manual(name = "Standard", values = standard_linetypes) +
+  scale_x_continuous(limits = c(0, 300), breaks = seq(0, 300, by = 50)) +
+        theme_classic() +
+        theme(axis.text = element_text(size = 12, color = "black"),
+              axis.title = element_text(size = 12, color = "black"),
+              plot.title = element_text(color = "black",
+                                   face = "bold"),
+              legend.text = element_text(size = 12, color = "black"),
+              legend.title = element_text(size = 12, color = "black")
+        ) +
+        labs(y = chla_RFU_title,
+             x = 'fDOM (QSU)')
+
+b_bw <- stds_gtm %>% 
+        ggplot(aes(x = avg_fdom_qsu, y = corrected_avg_chl_rfu,
+                   linetype = standard,
+                   shape = standard,
+                   group = standard_run)) +
+        geom_point(size = 2) +
+        stat_smooth(method = "lm",
+                    se = FALSE,
+                    fullrange = F,
+                    color = "black") +
+        scale_shape_manual(name = "Standard", values = standard_shapes) +
+        scale_linetype_manual(name = "Standard", values = standard_linetypes) +
+        theme_bw() +
+        theme(axis.text = element_text(size = 12, color = 'black'),
+              axis.title = element_text(size = 12)) +
+        labs(y = chla_RFU_title,
+             x = 'fDOM (QSU)')
+
+c_bw <- ambient_fdom %>%
+        filter(standard != "Humic") %>% 
+        ggplot(aes(x = avg_fdom_qsu, y = corrected_avg_chl_rfu,
+                   linetype = standard,
+                   shape = standard,
+                   group = standard_run)) +
+        geom_point() +
+        stat_smooth(method = "lm",
+                    se = FALSE,
+                    fullrange = F,
+                    color = "black",
+                    size = 0.5) +
+        scale_shape_manual(name = "Standard", values = standard_shapes) +
+  scale_x_continuous(breaks = seq(0, 300, by = 50), limits = c(0, 300)) +
+        scale_linetype_manual(name = "Standard", values = standard_linetypes) +
+        theme_classic() +
+        theme(axis.text = element_text(size = 12, color = "black"),
+              axis.title = element_text(size = 12, color = "black"),
+              plot.title = element_text(color = "black",
+                                   face = "bold"),
+              legend.text = element_text(size = 12, color = "black"),
+              legend.title = element_text(size = 12, color = "black")
+              ) +
+        labs(y = chla_RFU_title,
+             x = 'fDOM (QSU)')
+
+# to make one y -axix
+# fdom_p4 <- ggplot(data.frame(l = a_bw$labels$y, x = 1, y = 1)) +
+#   geom_text(aes(x, y, label = l), angle = 90, size = 5) + 
+#   theme_void() +
+#   coord_cartesian(clip = "off")
+
+fdom_multiplot_bw <- (a_bw + 
+                      theme(legend.position = "none", axis.text.x = element_blank()) + 
+                      labs(title = "A", x = "")) / 
+  (c_bw + theme(legend.position = "bottom") +
+     labs(title = "B"))
+
+ggsave(fdom_multiplot_bw, filename = here('output', 'bw', 'fdom_multiplot.png'),
+       dpi = 300, height = 6, width = 5, units = "in")
